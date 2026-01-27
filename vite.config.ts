f01cefb111bf4014c918b45039728f1daf8544bd
triche <taylor.riche@gmail.com>
2026-01-26 18:25:33 -0600

    refactor: remove Vitest configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'node',
-    include: ['test/**/*.test.cjs'],
-  },
 })
